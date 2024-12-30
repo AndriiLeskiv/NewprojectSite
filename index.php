1248a67f57705f27fcaede1e6f8410ffb4d7fe81
Leskiv Andrii <58334364+AndriiLeskiv@users.noreply.github.com>
2024-12-30 17:53:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,292 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h1>
+                    <a href="/">My blog</a>
+                </h1>
+            </div>
+            <nav class="col-8">
+                <ul>
+                    <li><a href="#">Головна</a></li>
+                    <li><a href="#">Про нас</a></li>
+                    <li><a href="#">Послуги</a></li>
+                    <li>
+                        <a href="#"><i class="fa-solid fa-user"></i> Кабінет</a>
+                        <ul>
+                            <li><a href="#">Admin</a></li>
+                            <li><a href="#">Вихід</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+    <div class="row">
+        <h2 class="slider-title">Топ публікацій!</h2>
+    </div>
+    <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/img_1.jfif" class="d-block w-100" alt="...">
+                <div class="carousel-caption carousel-caption-hack d-none d-md-block">
+                    <h5>
+                        <a href="#">First slide label</a>
+                    </h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/img_2.jfif" class="d-block w-100" alt="...">
+                <div class="carousel-caption carousel-caption-hack d-none d-md-block">
+                    <h5>
+                        <a href="#">First slide label</a>
+                    </h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/img_4.jfif" class="d-block w-100" alt="...">
+                <div class="carousel-caption carousel-caption-hack d-none d-md-block">
+                    <h5>
+                        <a href="#">First slide label</a>
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+
+<div class="container">
+    <div class="content row">
+        <div class="main-content col-md-9 col-12">
+            <h2>Останні публікації</h2>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_1.jfif" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+            <div class="post row">
+                <div class="img col-12 col-md-4">
+                    <img src="img/img_3.png" alt="" class="img-thumbnail">
+                </div>
+                <div class="post_text col-12 col-md-8">
+                    <h3>
+                        <a href="">Круті статті по новій темі...</a>
+                    </h3>
+                    <i class="fa-solid fa-user"></i>Ім'я автора
+                    <i class="fa-solid fa-calendar"></i>Dec 30, 2024
+                    <p class="preview-text">
+                        Lorem ipsum dolor sit   Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="sidebar col-md-3 col-12">
+            <div class="section search">
+                <h3>Search Post</h3>
+                <form action="" method="post">
+                    <input type="text" name="search-term" class="text-input" placeholder="Search...">
+                </form>
+            </div>
+            <div class="section topics">
+                <h3>Category</h3>
+                <ul>
+                    <li><a href="#">Програмування</a></li>
+                    <li><a href="#">Дизайн</a></li>
+                    <li><a href="#">Візуальзіція</a></li>
+                    <li><a href="#">Кейси</a></li>
+                    <li><a href="#">Мотивація</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="footer container-fluid">
+    <div class="footer-content container">
+        <div class="row">
+            <div class="footer-section about col-md-4 col-12">
+                <h3 class="logo-text">Мій блог</h3>
+                <p>Мій блог - цей блог зроблено для навчання і загального розвитку і для заробітку додаткової карми</p>
+                <div class="contact">
+                    <span><i class="fas fa-phone"></i>123-456-789</span>
+                    <span><i class="fas fa-envelope"></i>info@myBlog.com</span>
+                </div>
+                <div class="social">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+
+            <div class="footer-section links col-md-4 col-12">
+                <h3>Швидкий лінк</h3>
+                <br>
+                <ul>
+                    <li><a href="#">Події</a></li>
+                    <li><a href="#">Команда</a></li>
+                    <li><a href="#">Завдання</a></li>
+                    <li><a href="#">Інше</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-section contact-form col-md-4 col-12">
+                <h3>Контакти</h3>
+                <br>
+                <form action="#" method="post">
+                    <input type="email" name="email" class="text-input contact-input" placeholder="Your email address..">
+                    <textarea name="message" rows="4" cols="20" class="text-input contact-input" placeholder="Your message.."></textarea>
+                    <button type="submit" class="btn btn-big contact-btn">
+                        <i class="fas fa-envelope"></i>
+                        Відправити
+                    </button>
+                </form>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            &copy; myBlog.com | Designed by Leskiv
+        </div>
+    </div>
+</footer>
+
+<script src="https://kit.fontawesome.com/29f71dbbd1.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
